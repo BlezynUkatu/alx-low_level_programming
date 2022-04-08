779ed24079b6a31c1622bef4e8f90ff638b63c1f
BlezynUkatu <blessingukatu1@gmail.com>
2022-04-08 10:35:32 -0700

    my commit message

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * * main - main block
- * * Return: 0
- * */
+ * main - main block
+ * Return: 0
+ *
+*/
 int main(void)
 {
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
