bfed74cd606609d6eb88c5a35c14f4dd9f31aa97
BlezynUkatu <blessingukatu1@gmail.com>
2022-04-08 11:50:24 -0700

    My zero task commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * * main - prints is posetive, negative
+ * * or zero
+ * *Return: returns zero if program exits well.
+ *  */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+		return (0);
+}
