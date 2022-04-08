e2f403e2fc970e9ca7c6cd8a735c089ae2e0b81c
BlezynUkatu <blessingukatu1@gmail.com>
2022-04-08 09:54:18 -0700

    My 4th task commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ *
+ * Return: o after printing the function
+ */
+int main(void)
+[
+puts("\"programming is like building a multilingual puzzle"):
+return (o):
+]
