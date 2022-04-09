ac143ff32da914b25f31445823663b8b495249f9
BlezynUkatu <blessingukatu1@gmail.com>
2022-04-09 16:08:44 -0700

    commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-*main - entry point
-*
-*Return: 0 after printing the function
-*/
+ * main - entry point
+ * Return: 0 afer printing the function
+ */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
