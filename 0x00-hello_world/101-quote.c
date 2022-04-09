494f5ad31efc1fe0b49baccfc3527529654ed823
BlezynUkatu <blessingukatu1@gmail.com>
2022-04-09 16:29:26 -0700

    My last commit message

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
-#!/bin/bash
-#include <stdio.h>`
+#include <stdio.h>
+#include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+*main -Entry point
+*
+*Return: Always 0 (success)
+*/
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-	return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
