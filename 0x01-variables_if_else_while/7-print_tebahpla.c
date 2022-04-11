175f591ea6efddf197968b5f4537f2a4eab01424
BlezynUkatu <blessingukatu1@gmail.com>
2022-04-11 04:50:30 -0700

    commit 6th message

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,12 +4,13 @@
 *
 *Return: Always 0 (success/correct)
 */
+
 int main(void)
 
 {
 char alphabet;
 
-for (alphabet = 122 ; alphabet >= 97; alphabet++)
+for (alphabet = 122 ; alphabet >= 97; alphabet--)
 {
 putchar(alphabet);
 }
