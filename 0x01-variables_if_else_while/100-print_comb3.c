beb8925ff95741465aa2e0a94e291a22902a2ce6
BlezynUkatu <blessingukatu1@gmail.com>
2022-04-11 05:58:58 -0700

    commit message

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,7 @@
 *main - prints all possible combinations of two different digits,
 *       in ascending order, seperated by a comma follwed by a space.
 *
-*Return: Always 0 (success/correct)
+*Return: Always 0,
 */
 int main(void)
 {
@@ -13,8 +13,8 @@ for (digit1 = 0; digit < 9; digit1++)
 {
 for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
-putchar(digit1 % 10) + '0';
-putchar(digit2 % 10) + '0';
+putchar(digit1 % 10) + '0');
+putchar(digit2 % 10) + '0');
 
 if (digit1 == 8 && digit2 == 9)
 continue;
