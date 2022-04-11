ba58e176fc3bb37fd817a31be45c328c8bc15241
BlezynUkatu <blessingukatu1@gmail.com>
2022-04-11 04:11:07 -0700

    commit message

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,14 +12,13 @@ char alphabet;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 if (alphabet == 'q')
-continue; 
+continue;
 
 else if (alphabet == 'e')
 continue;
 
 putchar(alphabet); 
 }
-
 putchar('\n');
 
 return (0);
