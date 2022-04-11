ba37d5c30b03a2e0b949acc35df2ced7a6fb4994
BlezynUkatu <blessingukatu1@gmail.com>
2022-04-11 04:37:25 -0700

    6th commit message

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (success/correct)
+*/
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet =122 ; alphabet >= 97; alphabet++)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0)
+
+}
