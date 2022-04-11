8a3acec64d7890edac50e96576b818f83793ad17
BlezynUkatu <blessingukatu1@gmail.com>
2022-04-11 05:46:17 -0700

    10th task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+*main - prints all possible cominations of two different digits,
+*       in ascending order, seperated by a comma follwed by a space.
+*
+*Return: Always 0 (success/correct)
+*/
+int main(void)
+{
+int digit1, digit2;
+
+for (digit1 = 0; digit < 9; digit1++0
+{
+for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+{
+putchar(digit1 % 10) + '0');
+putchar(digit2 % 10) + '0');
+
+if (digit1 == 8 && digit2 == 9)
+continue;
+
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+
+return (0);
+}
