a811976dca6fc90b6f032ab01b4e0d1b4f74812d
BlezynUkatu <blessingukatu1@gmail.com>
2022-04-11 05:05:34 -0700

    9th commit message

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (success/correct)
+*/
+int main(void)
+
+{
+int number;
+
+for (number = 48; number <= 57; number++)
+{
+putchar(number);
+
+if (number == 57)
+{
+break;
+}
+
+putchar(',');
+putchar (' ');
+}
+
+putchar('\n');
+
+return (0);
+
+}
