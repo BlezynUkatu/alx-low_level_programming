0823f34f78905f5ea15c58751bd624a9cb354b19
BlezynUkatu <blessingukatu1@gmail.com>
2022-04-12 16:26:16 -0700

    1st task commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,18 +3,18 @@
 /**
  * print_alphabet - print alphabet lowercase
  *
- * Return: Always 0.
-*/
+ *Return: Always 0.
+ */
 
 void print_alphabet(void)
 {
-	char alphabet;
+char alphabet;
 
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	{
-		_putchar(alphabet);
-	}
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
 
-	_putchar('\n');
+_putchar('\n');
 
 }
