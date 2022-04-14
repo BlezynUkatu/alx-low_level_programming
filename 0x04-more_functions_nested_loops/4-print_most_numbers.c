2aa0feb146ecf66a4798dbc88620feafdb463c14
BlezynUkatu <blessingukatu1@gmail.com>
2022-04-14 06:59:55 -0700

    4TH commit message

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,18 @@
-#include "maiin.h"
+#include "main.h"
 /**
-* print_most_numbers - print numbers
-*
-*Return: Always 0.
-*/
+ * print_most_numbers - print numbers
+ *
+ * Return: Always 0.
+ */
 void print_most_numbers(void)
 {
 
-
 int i;
 
-i = 0
+i = 0;
 while (i < 10)
 {
-if (i !=2 && i != 4)
+if (i != 2 && i != 4)
 {
 _putchar (i + '0');
 }
