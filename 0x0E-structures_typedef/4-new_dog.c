a4a9d6872d2bf13a542380034a862ca0f97a7277
BlezynUkatu <blessingukatu1@gmail.com>
2022-05-11 04:56:02 -0700

    A door is what a dog is perpetually on the wrong side

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,41 +2,6 @@
 #include "dog.h"
 
 /**
- * _copy  -   Make a copy of passed in argument
- * @src:      Data to make copy of
- * Return:    Pointer
- */
-
-char *_copy(char *src)
-{
-	char *ptr;
-	int i, len;
-
-	if (src == NULL)
-	{
-		return (NULL);
-	}
-
-	for (len = 0; src[len] != '\0'; len++)
-		;
-
-	ptr = malloc(sizeof(char) * (len + 1));
-
-	if (ptr == NULL)
-	{
-		return (NULL);
-	}
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		ptr[i] = src[i];
-	}
-
-	ptr[i] = '\0';
-	return (ptr);
-}
-
-/**
  * new_dog     - Create a new dog variable
  * @name:        Name of the dog
  * @age:         Age of the dog
@@ -80,4 +45,3 @@ dog_t *new_dog(char *name, float age, char *owner)
 	(*snoopie).owner = new_owner;
 
 	return (snoopie);
-}
