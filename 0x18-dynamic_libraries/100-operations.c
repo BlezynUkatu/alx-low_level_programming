ac786b992a94c8fa32983232af09746ba7b86f2f
BlezynUkatu <blessingukatu1@gmail.com>
2022-07-16 16:16:53 -0700

    task 2

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,51 +1,26 @@
-#include <stdio.h>                                                                                                                    
-                                                                                                                                
+#include <stdio.h>
 int add(int a, int b){                                                                                                               
-	        int sum = a + b;                                                                                                              
-		        return sum;                                                                                                                   
+		        int sum = a + b;                                                                                                              
+					        return sum;                                                                                                                   
 }                                                                                                                                     
                                                                                                                                       
 int sub(int a, int b){                                                                                                               
-	        int difference = a - b;                                                                                                       
-		        return difference;                                                                                                            
+		        int difference = a - b;                                                                                                       
+					        return difference;                                                                                                            
 }                                                                                                                                     
                                                                                                                                       
 int mul(int a, int b){                                                                                                               
-	        int multiply = a * b;                                                                                                         
-		        return multiply;                                                                                                              
+		        int multiply = a * b;                                                                                                         
+					        return multiply;                                                                                                              
 }                                                                                                                                     
                                                                                                                                       
 int div(int a, int b){                                                                                                             
-	        float divide = a / b;                                                                                                         
-		        return divide;                                                                                                                
+		        float divide = a / b;                                                                                                         
+					        return divide;                                                                                                                
 }                                                                                                                                     
                                                                                                                                       
 int mod(int a, int b){                                                                                                             
-	        int rem = a % b;                                                                                                              
-		        return rem;                                                                                                                   
-}                    #include <stdio.h>                                                                                                                    
-                                                                                                                                
-int add(int a, int b){                                                                                                               
-	        int sum = a + b;                                                                                                              
-		        return sum;                                                                                                                   
-}                                                                                                                                     
-                                                                                                                                      
-int sub(int a, int b){                                                                                                               
-	        int difference = a - b;                                                                                                       
-		        return difference;                                                                                                            
-}                                                                                                                                     
-                                                                                                                                      
-int mul(int a, int b){                                                                                                               
-	        int multiply = a * b;                                                                                                         
-		        return multiply;                                                                                                              
-}                                                                                                                                     
-                                                                                                                                      
-int div(int a, int b){                                                                                                             
-	        float divide = a / b;                                                                                                         
-		        return divide;                                                                                                                
-}                                                                                                                                     
-                                                                                                                                      
-int mod(int a, int b){                                                                                                             
-	        int rem = a % b;                                                                                                              
-		        return rem;                                                                                                                   
-)
+		        int rem = a % b;                                                                                                              
+					        return rem;                                                                                                                   
+}                    
+
