4a622ef558f851883c81626e7d127d7381a0cf04
BlezynUkatu <blessingukatu1@gmail.com>
2022-07-16 16:07:19 -0700

    task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,51 @@
+#include <stdio.h>                                                                                                                    
+                                                                                                                                
+int add(int a, int b){                                                                                                               
+	        int sum = a + b;                                                                                                              
+		        return sum;                                                                                                                   
+}                                                                                                                                     
+                                                                                                                                      
+int sub(int a, int b){                                                                                                               
+	        int difference = a - b;                                                                                                       
+		        return difference;                                                                                                            
+}                                                                                                                                     
+                                                                                                                                      
+int mul(int a, int b){                                                                                                               
+	        int multiply = a * b;                                                                                                         
+		        return multiply;                                                                                                              
+}                                                                                                                                     
+                                                                                                                                      
+int div(int a, int b){                                                                                                             
+	        float divide = a / b;                                                                                                         
+		        return divide;                                                                                                                
+}                                                                                                                                     
+                                                                                                                                      
+int mod(int a, int b){                                                                                                             
+	        int rem = a % b;                                                                                                              
+		        return rem;                                                                                                                   
+}                    #include <stdio.h>                                                                                                                    
+                                                                                                                                
+int add(int a, int b){                                                                                                               
+	        int sum = a + b;                                                                                                              
+		        return sum;                                                                                                                   
+}                                                                                                                                     
+                                                                                                                                      
+int sub(int a, int b){                                                                                                               
+	        int difference = a - b;                                                                                                       
+		        return difference;                                                                                                            
+}                                                                                                                                     
+                                                                                                                                      
+int mul(int a, int b){                                                                                                               
+	        int multiply = a * b;                                                                                                         
+		        return multiply;                                                                                                              
+}                                                                                                                                     
+                                                                                                                                      
+int div(int a, int b){                                                                                                             
+	        float divide = a / b;                                                                                                         
+		        return divide;                                                                                                                
+}                                                                                                                                     
+                                                                                                                                      
+int mod(int a, int b){                                                                                                             
+	        int rem = a % b;                                                                                                              
+		        return rem;                                                                                                                   
+)
